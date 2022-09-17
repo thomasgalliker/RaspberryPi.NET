671cfbd44388b95120186ae4adaca697ef6575b7
Thomas Galliker <thomas@bluewin.ch>
2022-09-17 15:33:50 +0200

    Add unit test project

---
+++ Tests/RaspberryPi.NET.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace RaspberryPi.NET.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            // TODO Do some test...
+        }
+    }
+}
